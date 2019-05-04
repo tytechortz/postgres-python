75c50ab0a84664449149429eb964f60be1fb8003
James Swank <jmswank7@gmail.com>
2019-05-04 11:12:29 -0600

    filter method

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-class Movie
-    
+from movie import Movie 
+from user import User
+
+
+user = User("Jose")
+
+my_movie = Movie("The Matrix", "Sci-Fi", True)
+
+user.movies.append(my_movie)
+
+print(user.watched_movies())
