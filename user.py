7d04b7d4b88e18c0a881ee760fc05bfca335a683
James Swank <jmswank7@gmail.com>
2019-05-04 11:26:23 -0600

    delete movie

--- user.py
+++ user.py
@@ -1,3 +1,5 @@
+from Movie import Movie
+
 class User:
     def __init__(self,name):
         self.name = name
@@ -5,6 +7,13 @@ class User:
 
     def __repr__(self):
         return "<User {}>".format(self.name)
+
+    def add_movie(self,name,genre):
+        movie = Movie(name, genre, False)
+        self.movies.append(movie)
+
+    def delete_movie(self, name):
+        self.movies = list(filter(lambda movie: movie.name != name, self.movies))
 
     def watched_movies(self):
         return list(filter(lambda movie: movie.watched, self.movies))
