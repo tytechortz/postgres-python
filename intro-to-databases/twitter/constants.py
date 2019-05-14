782f9b5cff87f0069a5aac7e49f5dd808c72964d
James Swank <jmswank7@gmail.com>
2019-05-14 11:28:11 -0600

    keys in env variables

--- intro-to-databases/twitter/constants.py
+++ intro-to-databases/twitter/constants.py
@@ -1,8 +1,12 @@
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
+import os
+
+CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
 
 REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
 ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
 AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
 
+print(CONSUMER_KEY)
+print(CONSUMER_SECRET)
 
--- intro-to-databases/twitter/login.py
+++ intro-to-databases/twitter/login.py
@@ -1,4 +1,7 @@
 import constants
 import oauth2
 
-consumer = oauth2.Consumer()
+consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
+client = oauth2.Client(consumer)
+
+client.requests()
