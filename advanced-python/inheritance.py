8174604d7551af35a1cda9688640b13bfe0ec018
James Swank <jmswank7@gmail.com>
2019-05-15 12:35:37 -0600

    ends twitter, starts advanced python

---
+++ advanced-python/inheritance.py
@@ -0,0 +1,19 @@
+class Decimal:
+    def __init__(self, number, places):
+        self.number = number
+        self.places = places
+
+    def __repr__(self):
+        return "%.{}f".format(self.places) % self.number
+
+
+class Currency(Decimal):
+    def __init__(self, symbol, number, places):
+        super().__init__(number, places)
+        self.symbol = symbol 
+
+    def __repr__(self):
+        return "{}{}".format(self.symbol, super().__repr__())
+
+print(Decimal(15.6789, 2))
+print(Currency("$", 15.6789, 3))
