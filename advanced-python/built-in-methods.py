f1f4fc7fd8f69402b1f1f17b1d44b8ada423dc6d
James Swank <jmswank7@gmail.com>
2019-05-16 15:35:02 -0600

    convert pounds to usd

---
+++ advanced-python/built-in-methods.py
@@ -0,0 +1,18 @@
+class Currency:
+    def __init__(self, code, exchange_to_usd):
+        self.amount = 0.00
+        self.code = code
+        self.exchange_to_usd = exchange_to_usd
+
+    def set_amount(self, amount):
+        self.amount = amount
+
+    def in_currency(self, amount):
+        return amount / self.exchange_to_usd
+
+    def to_usd(self, amount=None):
+        pass  
+
+
+pounds = Currency("GBP", 1.21)
+print(pounds.in_currency(1000))
